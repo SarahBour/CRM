ad1253e7011568bb1ae51d0999f33aed4357eebb
SarahBour <bourenanesarah@gmail.com>
2024-11-12 09:36:52 +0000

    login enhanced.

--- Culture2Geth/Pages/Index.cshtml.cs
+++ Culture2Geth/Pages/Index.cshtml.cs
@@ -24,7 +24,7 @@ namespace Culture2Geth.Pages
 		public async Task<IActionResult> OnPostAsync(string Email, string Password)
 		{
 			var user = await _context.User.FirstOrDefaultAsync(u => u.Email == Email);
-            if (user != null && Password == user.Password)
+            if (user != null && Password == user.Password && user.ProfileStatus == "Approved")
 			{
                 if (user.Role == "Admin")
 				{
@@ -38,16 +38,18 @@ namespace Culture2Geth.Pages
 			}
 			else if(user == null)
 			{
-				ModelState.AddModelError(String.Empty, "Email not found");
+				ModelState.AddModelError(String.Empty, "Email incorrect or doesn't exist");
 			}
 			else if(Password != user.Password)
 			{
 				ModelState.AddModelError(String.Empty, "Incorrect password");
 			}
+			else if(user.ProfileStatus != "Approved")
+			{
+				return RedirectToPage("Success");
+			}
 			return Page();
 		}
 	}
 }
-// BCrypt.Net.BCrypt.Verify(Password, user.Password)
-// add the errors in the UI for the credentials 
-// forgot password logic 
+// Later: BCrypt.Net.BCrypt.Verify(Password, user.Password) 
